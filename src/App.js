93e2a14735d351aaf0b9ed4a6807911f7854e66e
jmolina518 <jmolina518@gmail.com>
2021-01-02 08:04:43 -0600

    render MovieList

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MovieList from './components/MovieList';
 
 const App = () => {
   const [movies, setMovies] = useState([
@@ -32,7 +33,9 @@ const App = () => {
 
   return (
     <div className='container-fluid movie-app'>
-      <div className='row'></div>
+      <div className='row'>
+        <MovieList movies={movies} />
+      </div>
     </div>
   );
 };
