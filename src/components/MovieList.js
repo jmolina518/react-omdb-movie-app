3189f547afc9d83d01be2ae2ea39e479179df26d
jmolina518 <jmolina518@gmail.com>
2021-01-02 10:24:05 -0600

    add overlay to MovieList component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -6,6 +6,9 @@ const MovieList = (props) => {
       {props.movies.map((movie, index) => (
         <div className='image-container d-flex justify-content-start m-3'>
           <img src={movie.Poster} alt='movie'></img>
+          <div className='overlay d-flex align-items-center justify-content-center'>
+            Add to Favourites
+          </div>
         </div>
       ))}
     </>
