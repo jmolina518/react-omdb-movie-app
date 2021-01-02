b05ad9ecf7e44c706884a0fe4a613059694703c7
jmolina518 <jmolina518@gmail.com>
2021-01-02 10:10:57 -0600

    update App.js with new components

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
   const getMovieRequest = async () => {
     const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
@@ -23,6 +26,10 @@ const App = () => {
 
   return (
     <div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+      </div>
       <div className='row'>
         <MovieList movies={movies} />
       </div>
